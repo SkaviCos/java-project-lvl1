6f0432082ea36e8432fdc12b494fe0f628fb0e64
skavicos <kskavinsky@skolopendra.com>
2024-01-08 16:28:26 +0300

    fix prime algorithm

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -19,7 +19,7 @@ public class Prime {
     }
 
     public static boolean isPrime(int number) {
-        for (int i = 2; i * i < number; i++) {
+        for (int i = 2; i * i <= number; i++) {
             if (number % i == 0) {
                 return false;
             }
